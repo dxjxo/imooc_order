9dba8cc849c017366dfa7799a4b7d48f1012ee8a
dxjxo <38204882+dxjxo@users.noreply.github.com>
2018-10-21 12:21:29 +0800

    Update static.py
    #app.logger.info(filename)  注销这句

--- web/controllers/user/static.py
+++ web/controllers/user/static.py
@@ -7,6 +7,6 @@ route_static = Blueprint('static_page',__name__)
 
 @route_static.route('/<path:filename>')
 def index(filename):
-    app.logger.info(filename)
-    print('filename',filename)
+    #app.logger.info(filename)
+    
     return send_from_directory(app.root_path+'/web/static/',filename)
