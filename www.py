e1cfbf556666254dfdfe638893b8cd5ccfc389a2
dxj <771426949@qq.com>
2018-10-21 19:02:48 +0800

    添加后台账号管理页面 account/accounty.py

--- www.py
+++ www.py
@@ -3,8 +3,10 @@ __author__ = 'Administrator'
 from application import app
 from web.controllers.index import route_index
 from web.controllers.user.user import route_user
+from web.controllers.account.account import route_account
 from web.controllers.user.static import route_static
 
 app.register_blueprint(route_index,url_prefix = '/')
 app.register_blueprint(route_user,url_prefix='/user')
 app.register_blueprint(route_static,url_prefix='/static')
+app.register_blueprint(route_account,url_prefix='/account')
