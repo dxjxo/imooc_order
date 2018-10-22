d815522f4209b84639b69b9224009d7a1d963bb5
dxj <771426949@qq.com>
2018-10-22 14:30:44 +0800

    对用户的登陆的密码进行加密 然后判断 ，登陆页面改为Ajax提示提升用户体验

--- common/libs/user/UserService.py
+++ common/libs/user/UserService.py
@@ -1,2 +1,14 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Administrator'
+import hashlib,base64
+class UserService():
+    """
+    对用户输入的密码加上随机字符串进行 md5 和 base64 加密
+    """
+    @staticmethod
+    def genPwd(pwd,salt):
+        # 初始化md5
+        m = hashlib.md5()
+        str = "%s-%s" %(base64.encodebytes(pwd.encode("utf-8")),salt)
+        m.update( str.encode('utf-8') )
+        return m.hexdigest()# 返回16进制
